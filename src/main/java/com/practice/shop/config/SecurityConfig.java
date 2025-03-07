7eb3ec8482602d488df13558d2899fc84a13f40f
kkh0920 <rudgns1535@gmail.com>
2025-03-07 12:42:07 +0900

    fix: enable csrf

--- src/main/java/com/practice/shop/config/SecurityConfig.java
+++ src/main/java/com/practice/shop/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,10 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(authorizeRequests -> {
-            authorizeRequests.requestMatchers("/**").permitAll();
-        });
+//        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests.requestMatchers("/**").permitAll()
+        );
         http.formLogin(formLogin -> formLogin.loginPage("/login")
                 .defaultSuccessUrl("/"));
         http.logout(logout -> logout.logoutUrl("/logout"));
